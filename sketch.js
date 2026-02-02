f8b8fc7b128ef0241a2d407709d3388443134b88
jtrajano <jlt212000@yahoo.com>
2026-02-02 06:57:59 +0000

    Add color palettes

--- sketch.js
+++ sketch.js
@@ -1,34 +1,59 @@
-function setup() {
-  createCanvas(400, 400);
-}
-
-function draw() {
-  background(220);
-}var fillColor = 'white'
+var fillColor = 'black';
+var colors = ['white', 'red', 'blue', 'green', 'yellow', 'orange', 'purple', 'black'];
+var paletteY = 20;
+var paletteX = 50;
+var colorSize = 50;
+var spacing = 10;
 
 function setup() {
   createCanvas(600, 600);
   background('#999999');
   noStroke();
-  
-  rect(50, 20, 60, 60);
-  fill('red');
-  rect(120, 20, 60, 60);
+  // Draw color palette once
+  drawPalette();
 }
 
 function draw() {
+  // Check which color is clicked
+  if (mouseIsPressed && mouseY < 100) {
+    checkColorClick();
+  }
+  
+  // Draw on canvas
+  fill(fillColor);
+  noStroke();
+  if(mouseIsPressed && mouseY > 100) {
+    ellipse(mouseX, mouseY, 40, 40);
+  }
+}
 
-  
-  if (mouseIsPressed && mouseX > 50 && mouseX < 110 && mouseY > 20 && mouseY < 70) {
-    fillColor = 'white'
-  }
+function drawPalette() {
+  noStroke();
+  for (let i = 0; i < colors.length; i++) {
+    fill(colors[i]);
+    rect(paletteX + i * (colorSize + spacing), paletteY, colorSize, colorSize);
     
-  if (mouseIsPressed && mouseX > 120 && mouseX < 180 && mouseY > 20 && mouseY < 70) {
-    fillColor = 'red'
+    // Highlight selected color with border
+    if (fillColor === colors[i]) {
+      stroke(0);
+      strokeWeight(3);
+      noFill();
+      rect(paletteX + i * (colorSize + spacing), paletteY, colorSize, colorSize);
+      noStroke();
+    }
   }
-  
-  fill(fillColor)
-  if(mouseIsPressed && mouseY > 110) {
-    ellipse(mouseX, mouseY, 60, 60);
+}
+
+function checkColorClick() {
+  if (mouseIsPressed) {
+    for (let i = 0; i < colors.length; i++) {
+      let x = paletteX + i * (colorSize + spacing);
+      let y = paletteY;
+      
+      if (mouseX > x && mouseX < x + colorSize && 
+          mouseY > y && mouseY < y + colorSize) {
+        fillColor = colors[i];
+      }
+    }
   }
 }
