46d98c6c3c5664909e61772933d4696ec2d7966c
jtrajano <jlt212000@yahoo.com>
2026-02-02 07:06:00 +0000

    Add erase button

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # hytel-paint-project
+
+### Add python uv
+```
+pip install uv
+```
+
+### Activate virtual python env
+
+```
+source .venv/bin/activate
+```
+
+### Running the projectc
+
+```
+cd /workspaces/hytel-paint-project
+python3 -m http.server 8000
+```
--- sketch.js
+++ sketch.js
@@ -4,6 +4,8 @@ var paletteY = 20;
 var paletteX = 50;
 var colorSize = 50;
 var spacing = 10;
+var eraserX = 550;
+var isEraser = false;
 
 function setup() {
   createCanvas(600, 600);
@@ -14,6 +16,12 @@ function setup() {
 }
 
 function draw() {
+  // Redraw palette area to update selection highlight
+  fill('#999999');
+  noStroke();
+  rect(0, 0, 600, 100);
+  drawPalette();
+  
   // Check which color is clicked
   if (mouseIsPressed && mouseY < 100) {
     checkColorClick();
@@ -34,7 +42,7 @@ function drawPalette() {
     rect(paletteX + i * (colorSize + spacing), paletteY, colorSize, colorSize);
     
     // Highlight selected color with border
-    if (fillColor === colors[i]) {
+    if (fillColor === colors[i] && !isEraser) {
       stroke(0);
       strokeWeight(3);
       noFill();
@@ -42,10 +50,35 @@ function drawPalette() {
       noStroke();
     }
   }
+  
+  // Draw eraser button
+  fill(255);
+  stroke(0);
+  strokeWeight(2);
+  rect(eraserX, paletteY, colorSize, colorSize);
+  
+  // Draw eraser icon
+  fill(220);
+  noStroke();
+  rect(eraserX + 10, paletteY + 15, 30, 20);
+  fill(0);
+  textAlign(CENTER, CENTER);
+  textSize(10);
+  text('ERASE', eraserX + 25, paletteY + 40);
+  
+  // Highlight eraser if selected
+  if (isEraser) {
+    stroke(0);
+    strokeWeight(3);
+    noFill();
+    rect(eraserX, paletteY, colorSize, colorSize);
+    noStroke();
+  }
 }
 
 function checkColorClick() {
   if (mouseIsPressed) {
+    // Check color palette
     for (let i = 0; i < colors.length; i++) {
       let x = paletteX + i * (colorSize + spacing);
       let y = paletteY;
@@ -53,7 +86,15 @@ function checkColorClick() {
       if (mouseX > x && mouseX < x + colorSize && 
           mouseY > y && mouseY < y + colorSize) {
         fillColor = colors[i];
+        isEraser = false;
       }
+    }
+    
+    // Check eraser button
+    if (mouseX > eraserX && mouseX < eraserX + colorSize &&
+        mouseY > paletteY && mouseY < paletteY + colorSize) {
+      isEraser = true;
+      fillColor = '#999999'; // Set to background color
     }
   }
 }
